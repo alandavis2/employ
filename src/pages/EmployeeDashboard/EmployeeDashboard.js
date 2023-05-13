cbb5db3f62d71f75f7fbb936dc32855c261879b4
unknown <alandavis.ad21@jecc.ac.in>
2023-05-13 11:35:00 +0530

    added table and form

--- src/pages/EmployeeDashboard/EmployeeDashboard.js
+++ src/pages/EmployeeDashboard/EmployeeDashboard.js
@@ -1,7 +1,61 @@
 import React from "react";
 
-const EmployeeDashboard = () => {
-  return <div className="h-full">this is Dashboard</div>;
-};
+function EmployeeDashboard () {
+  return (
+  <form>
+    <div>
+  <label class="block">
+    <span class="block text-sm font-medium text-slate-700">Username</span>
+    
+    <input type="text" value="enter your username" disabled class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "/>
+  </label>
+  <input type="Name" className="form-control" id="exampleInputId" aria-describedby="emailHelp" placeholder="Enter Field Name" />
+  </div>
+  <div>
+  <label class="block">
+    <span class="block text-sm font-medium text-slate-700">Designation</span>
+    
+    <input type="text" value="enter your Designation" disabled class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "/>
+  </label>
+  <input type="Name" className="form-control" id="exampleInputId" aria-describedby="emailHelp" placeholder="Enter Field Dsignation" />
+  </div>
+  <div>
+  <label class="block">
+    <span class="block text-sm font-medium text-slate-700">Location</span>
+    
+    <input type="text" value="enter your Location or place" disabled class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "/>
+  </label>
+  <input type="Name" className="form-control" id="exampleInputId" aria-describedby="emailHelp" placeholder="Enter Field Location" />
+  </div>
+  <div>
+  <label class="block">
+    <span class="block text-sm font-medium text-slate-700">salary</span>
+    
+    <input type="text" value="enter your salary" disabled class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "/>
+  </label>
+  <input type="Name" className="form-control" id="exampleInputId" aria-describedby="emailHelp" placeholder="Enter Field Salary" />
+  </div>
+</form>
+)};
 
 export default EmployeeDashboard;
--- src/pages/EmployeeForm/EmployeeForm.js
+++ src/pages/EmployeeForm/EmployeeForm.js
@@ -1,7 +1,39 @@
-import React from "react";
 
-const EmployeeForm = () => {
-  return <div className="h-full">this is employee form</div>;
-};
+import React, { useEffect, useState } from 'react';
+function Home() {
+  const [users, setUsers] = useState([]);
 
-export default EmployeeForm;
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {users.map(user => (
+              <tr key={user.id}>
+                <td className="px-6 py-4 whitespace-nowrap">{user.id}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{user.name}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      
+    </div>
+  );
+}
+
+export default Home;
